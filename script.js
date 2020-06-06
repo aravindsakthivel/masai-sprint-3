26ff509361071ee358e120833e60b68a50fe2569
aravindsakthivel <aravind.sakthi@outlook.com>
2020-06-06 14:50:34 +0530

    third commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var valueHolder = []
 
 
 function inputOperation(){
-    var inputData = document.getElementById('coin_name').value
+    var inputData = document.getElementById('coin_name').value.toLowerCase()
     valueHolder.push(inputData)
     getCryptoData(inputData)
 }
@@ -20,9 +20,36 @@ function getCryptoData(data){
 }
 
 function processData(){
-    console.log(this)
-    var dataBox = document.getElementById('result')
-    
+    // console.log(this)
+    // console.log(this.market_data.market_cap.usd)
+    var result = document.getElementById('result')
+    result.innerText = ""
+    var block = document.createElement('div')
+
+    var name_box = document.createElement('div')
+    var name = document.createElement('p')
+    name.innerText = this.id.toUpperCase()
+    var logo = document.createElement('img')
+    logo.setAttribute('src', this.image.thumb)
+    name_box.append(name, logo)
+
+    var info_box = document.createElement('div')
+    var rank = document.createElement('p')
+    rank.innerText = "Rank: " + this.market_cap_rank
+
+    var coinPrice = document.createElement('p')
+    coinPrice.innerText = '1 ' + this.id + ' in USD: ' + this.market_data.current_price.usd
+
+    var marketCap = document.createElement('p')
+    marketCap.innerText = 'Market capitalization in USD: ' + this.market_data.market_cap.usd
+
+    var description = document.createElement('p')
+    description.innerText = this.description.en
+    info_box.append(rank, coinPrice, marketCap, description)
+
+    block.append(name_box, info_box)
+    result.append(block)
+
 }
 
 
