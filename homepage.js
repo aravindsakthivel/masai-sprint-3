e206d3af04f3aa9b9476ef7d042ae6a5ee4a17a5
aravindsakthivel <aravind.sakthi@outlook.com>
2020-06-07 11:17:49 +0530

    sixth commit

--- homepage.js
+++ homepage.js
@@ -1,14 +1,33 @@
 var searchHolder = []
 var acquiredData
+var curOpt
 
 
 function inputOperation(){
     var inputData = document.getElementById('coin_name').value.toLowerCase()
     searchHolder.push(inputData)
     getCryptoData(inputData)
+    comparisonMoreinfo(inputData.toUpperCase())
+
 }
 
+function comparisonMoreinfo(data){
+    var comparePage = document.getElementById('comparison')
+    comparePage.innerHTML = ""
+    var comBtn = document.createElement('button')
+    comBtn.textContent = 'Do you want to compare'
+    comBtn.setAttribute('onclick', 'compareCoinPre()')
+
+    var infoBtn = document.createElement('button')
+    infoBtn.textContent = 'More info about ' + data
+    infoBtn.setAttribute('onclick', 'moreInfo()')
+    comparePage.append(infoBtn, comBtn)
+}
+
+
 function getCryptoData(data){
+    var displayBlock = document.getElementById('more_info')
+    displayBlock.innerHTML = ""
     var xhrcrypto = new XMLHttpRequest()
     var cryptoApi = 'https://api.coingecko.com/api/v3/coins/'
     // console.log(cryptoApi + data + "?market_data=true")
@@ -17,6 +36,7 @@ function getCryptoData(data){
     xhrcrypto.onload = function(){
         if(this.status === 200){
             acquiredData = JSON.parse(this.response)
+            // console.log(acquiredData)
             processData.call(acquiredData)
         }
         else if(this.status === 404){
@@ -50,7 +70,7 @@ function processData(curncy){
     coinValue.setAttribute('id', 'coin_value')
     var coinPrice = document.createElement('p')
     var otherValue = this.market_data.current_price[curncy] || this.market_data.current_price.usd
-    console.log(this.market_data.current_price[curncy])
+    // console.log(this.market_data.current_price[curncy])
     var preference = 'USD'
     if (curncy === undefined){
         preference = 'USD'
@@ -58,7 +78,7 @@ function processData(curncy){
     else{
         preference = curncy.toUpperCase()
     }
-    coinPrice.innerText = '1 ' + this.id + ' in '+ preference + " " + otherValue
+    coinPrice.innerText = '1 ' + this.id + ' in '+ preference + ": " + otherValue
     var currencySelect = document.createElement('select')
     currencySelect.setAttribute('id', 'currency_option')
     currencySelect.setAttribute('onchange', 'changeCurrency()')
@@ -86,9 +106,87 @@ function processData(curncy){
 
 
 function changeCurrency(){
-    var curOpt = document.getElementById('currency_option').value
-    console.log(acquiredData)
+    curOpt = document.getElementById('currency_option').value
     processData.call(acquiredData, curOpt)
+}
+
+
+function compareCoinPre(){
+    var comparePage = document.getElementById('comparison')
+    comparePage.innerHTML = ""
+    var otherInput = document.createElement('input')
+    otherInput.setAttribute('id', 'compVal')
+    var otherBtn = document.createElement('button')
+    otherBtn.setAttribute('onclick', 'getCompareDataPre()')
+    otherBtn.textContent = 'Find'
+    comparePage.append(otherInput, otherBtn)
+}
+
+
+function moreInfo(){
+    var displayBlock = document.getElementById('more_info')
+    displayBlock.innerHTML = ""
+    var infoChange = document.createElement('div')
+    var lowValue = document.createElement('p')
+    var chooseCurrency = curOpt || 'usd'
+    lowValue.innerText = "Lowest drop in last 24 hours " + chooseCurrency.toUpperCase() + ": " + acquiredData.market_data.low_24h[chooseCurrency]
+
+    var highValue = document.createElement('p')
+    highValue.textContent = "Highest growth in last 24 hours " + chooseCurrency.toUpperCase() + ": " + acquiredData.market_data.high_24h[chooseCurrency]
+
+    var maxCap = document.createElement('p')
+    maxCap.innerText = "Total supply in nos: " + acquiredData.market_data.total_supply
+
+    var circulation = document.createElement('p')
+    circulation.textContent = "circulating supply in nos: " + acquiredData.market_data.circulating_supply
+
+    infoChange.append(lowValue, highValue, maxCap, circulation)
+    infoChange.setAttribute('class', 'info_change')
+
+    var desc = acquiredData.description.en
+    var box = document.createElement('div')
+    box.setAttribute('class', 'description')
+    box.innerText = desc
+    displayBlock.append(infoChange, box)
+
+}
+
+
+function getCompareDataPre(){
+    var data = document.getElementById('compVal').value
+    console.log(data)
+    var xhrCryptoCom = new XMLHttpRequest()
+    var cryptoApi = 'https://api.coingecko.com/api/v3/coins/'
+    // console.log(cryptoApi + data + "?market_data=true")
+    xhrCryptoCom.open("GET", cryptoApi + data + "?market_data=true")
+    xhrCryptoCom.send()
+    xhrCryptoCom.onload = function(){
+        if(this.status === 200){
+            var compareData = JSON.parse(this.response)
+            console.log(compareData)
+            compareCoin.call(compareData)
+        }
+        else if(this.status === 404){
+            comparePage.innerHTML = 'Sorry it is not a Cryptocurrency'
+            comparePage.setAttribute('style', 'color:white')
+        }
+    }
+}
+
+
+function compareCoin(){
+    var result = document.getElementById('result')
+    result.innerText = ""
+    var comparePage = document.getElementById('comparison')
+    comparePage.innerText = ""
+    var compBlock = document.createElement('div')
+    compBlock.setAttribute('id', 'compare_op')
+    var firstCoinValue = document.createElement('p')
+    firstCoinValue.textContent = "The value of one " + acquiredData.id.toUpperCase() + " in USD : "+ acquiredData.market_data.current_price.usd
+    var secondCoinValue = document.createElement('p')
+    secondCoinValue.textContent = "The value of one " + this.id.toUpperCase() + " in USD : "+ this.market_data.current_price.usd
+    compBlock.append(firstCoinValue, secondCoinValue)
+    result.append(compBlock)
 }
 
 
@@ -99,13 +197,12 @@ function totalCoins(){
     xhrCoins.send()
     xhrCoins.onload = function(){
         var coinData = JSON.parse(this.response)
-        console.log(Object.keys(coinData).length)
+        // console.log(Object.keys(coinData).length)
         var result = document.getElementById('result')
         result.innerText = 'There is a total of ' + Object.keys(coinData).length + ' Cryptocurrency (approx)'
         result.setAttribute('style' , "color:white")
     }
 }
-
 
 
 
